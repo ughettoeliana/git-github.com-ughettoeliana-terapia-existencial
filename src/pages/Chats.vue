029cf8a6857d960dde2d24415d4990a469bc301b
Eliana Ughetto <elianaughetto10@gmail.com>
2023-10-22 16:16:52 -0300

    deleted unused code, and cleared up the html

--- src/pages/Chats.vue
+++ src/pages/Chats.vue
@@ -4,7 +4,7 @@ import BaseLabel from "../components/BaseLabel.vue";
 import BaseInput from "../components/BaseInput.vue";
 import BaseTextarea from "../components/BaseTextarea.vue";
 import Loader from "../components/Loader.vue";
-import { getUserProfileById, getUsers } from '../services/user';
+import { getUsers } from '../services/user';
 
 
 export default {
@@ -14,7 +14,6 @@ export default {
         return {
             chatsLoading: true,
             users: [],
-            loggedUser: {}
         }
     },
     methods: {
@@ -27,8 +26,7 @@ export default {
     },
     async mounted() {
         this.getUsers();
-        // this.loggedUser = await getUserProfileById(this.users.id);
-        // console.log('usuario logeado:', this.loggedUser.rol)// Llama a la función para obtener los usuarios
+
     },
 };
 </script>
@@ -39,27 +37,12 @@ export default {
     <template v-else>
         <div class=" min-vh-100">
             <div class="card mx-auto my-4" style="width: 20rem;">
-            <!-- <template v-if="loggedUser.rol === 'admin'"> -->
-                    <div class="list-group list-group-flush" v-for="user in users" :key="user.id">
-
-                        <span> (Admin)</span>
-                        <div class="list-group-item my-3 fw-bold">{{ user.email }} <router-link
-                                :to="`/usuario/${user.id}/chat`" class="btn btn-info text-black mx-3 my-3">Ir al
-                                chat</router-link>
-                        </div>
+                <div class="list-group list-group-flush" v-for="user in users" :key="user.id">
+                    <div class="list-group-item my-3 fw-bold">{{ user.email }} <router-link :to="`/usuario/${user.id}/chat`"
+                            class="btn btn-info text-black mx-3 my-3">Ir al
+                            chat</router-link>
                     </div>
-                <!-- </template> -->
-                <template >
-                    <div class="card mx-auto my-4" style="width: 20rem;">
-                        <div class="list-group list-group-flush">
-                            <div class="list-group-item my-3 fw-bold"><router-link
-                                    :to="`/usuario/1zIiuPCeTDXCaWu1JBnZIozLDG53/chat`"
-                                    class="btn btn-info text-black mx-3 my-3">Ir al chat con el Admin</router-link>
-                            </div>
-                        </div>
-                    </div>
-                </template>
-
+                </div>
             </div>
         </div>
     </template>
